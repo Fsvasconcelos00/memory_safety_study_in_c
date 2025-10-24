dcec1ea479c6825424b5ae29b779fa5ac125f4a2
Fernando Vasconcelos <fernandovasconcelos@Fernandos-MacBook-Air.local>
2025-10-23 21:48:35 -0300

    Dangling pointer

--- main.c
+++ main.c
@@ -2,10 +2,13 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-void random_pointerUsage(uint32_t* my_randomPointer)
+void dangling_pointerBomb(uint32_t** my_randomPointer)
 {
-    printf("Pointer value: %d\n", (*my_randomPointer));
-    (*my_randomPointer) = 1;
+    uint32_t temp_value = 10;
+
+    // temp_value is a local variable and won't exist when the function goes out of scope
+    // This will change the random_pointer address on the main function
+    (*my_randomPointer) = &temp_value;
 }
 
 int main(void)
@@ -13,12 +16,15 @@ int main(void)
     uint32_t random_value = 5;
     uint32_t* random_pointer = NULL;
 
+    
+    // Dereferencing uninitialized or NULL pointers
     // (*random_pointer) = 5; // This would result in a segmentation fault
-    random_pointer = &random_value; // This will fix the segmentation fault
+
+    random_pointer = &random_value; // Initializing the pointer will fix the segmentation fault
 
     printf("Pointer address: %p\n", random_pointer);
-    random_pointerUsage(random_pointer);
-    random_pointerUsage(random_pointer);
+    dangling_pointerBomb(&random_pointer);
+    printf("Pointer address: %p\n", random_pointer);
 
     return(0);
 }
